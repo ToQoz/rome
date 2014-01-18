03991e237c220f09565a76524dd3401a78bd5f8d
Takatoshi Matsumoto <toqoz403@gmail.com>
2014-01-18 19:57:55 +0900

    Add Router.[Get|Head|Post|Put|Delete]Func

--- rome.go
+++ rome.go
@@ -14,26 +14,53 @@ func NewRouter() *Router {
 	return &Router{tries: map[string]*trie{}}
 }
 
+// --- Routing Helper ---
+
+// GET
 func (router *Router) Get(pattern string, handler http.Handler) {
 	router.handle("GET", pattern, handler)
-
 	router.handle("HEAD", pattern, handler)
 }
 
+func (router *Router) GetFunc(pattern string, f func(http.ResponseWriter, *http.Request)) {
+	router.Get(pattern, http.HandlerFunc(f))
+	router.Head(pattern, http.HandlerFunc(f))
+}
+
+// HEAD
 func (router *Router) Head(pattern string, handler http.Handler) {
 	router.handle("HEAD", pattern, handler)
 }
 
+func (router *Router) HeadFunc(pattern string, f func(http.ResponseWriter, *http.Request)) {
+	router.Head(pattern, http.HandlerFunc(f))
+}
+
+// POST
 func (router *Router) Post(pattern string, handler http.Handler) {
 	router.handle("POST", pattern, handler)
 }
 
+func (router *Router) PostFunc(pattern string, f func(http.ResponseWriter, *http.Request)) {
+	router.Post(pattern, http.HandlerFunc(f))
+}
+
+// PUT
 func (router *Router) Put(pattern string, handler http.Handler) {
 	router.handle("PUT", pattern, handler)
 }
 
+func (router *Router) PutFunc(pattern string, f func(http.ResponseWriter, *http.Request)) {
+	router.Put(pattern, http.HandlerFunc(f))
+}
+
+// DELETE
 func (router *Router) Delete(pattern string, handler http.Handler) {
 	router.handle("DELETE", pattern, handler)
+}
+
+func (router *Router) DeleteFunc(pattern string, f func(http.ResponseWriter, *http.Request)) {
+	router.Delete(pattern, http.HandlerFunc(f))
 }
 
 func (router *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
