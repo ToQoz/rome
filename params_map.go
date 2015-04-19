b5f4df4b66e9b25fa073a097cefceba3497718e1
Takatoshi Matsumoto <toqoz403@gmail.com>
2015-04-20 01:13:06 +0900

    doc: tweak

--- params_map.go
+++ params_map.go
@@ -4,11 +4,11 @@ import (
 	"net/http"
 )
 
-// ParamsMap is params in URL path.
+var ParamsMap = map[*http.Request]Params{}
+
 // WARNING:
 //   This is NOT goroutine safe.
 //   Now, I'm going to make this goroutine safe.
-var ParamsMap = map[*http.Request]Params{}
 
 func setParams(r *http.Request, ps Params) {
 	ParamsMap[r] = ps
